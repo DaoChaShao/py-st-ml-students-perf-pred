fab183404da628781aea3fe154020d224efbc985
DaoChaShao <27882272@qq.com>
2025-11-27 16:12:20 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/11/27 16:09
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   main.py
+# @Desc     :   
+
+def main() -> None:
+    """ Main Function """
+    pass
+
+if __name__ == "__main__":
+    main()
